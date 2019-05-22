ccf3db028f026b82d3962e862f08f6a1dba69a03
Bruno <br.bz.bts@live.com>
2019-05-22 02:05:57 -0300

    Basic functionalities added

--- TesteFila/src/main.cpp
+++ TesteFila/src/main.cpp
@@ -3,69 +3,93 @@
 
 #include <Arduino.h>
 #include <QueueList.h>
-#include <Ticker.h>
+#include <Ticker.h> // Timer
 
-#define tempCont 0.2 //
+#define PUSH_INTERVAL 2 // Intervalo entre pacotes de pulsos
+#define SEND_INTERVAL 4 // Intervalo entre envios
+#define SENSOR_PIN D3   // Porta do sensor
 
-#define tempPush 5 //
-
-#define tempEmpty 8 //
-
-
-class dados{
-  public: 
-  int valor;
+struct data // Conjunto de informações a serem enviadas
+{
+  int value;
   unsigned long time;
 };
 
-int volatile cont = 0;
+volatile int pulseCounter = 0; // Contador de pulsos do sensor
 
-QueueList<dados> filaPulsos;
+unsigned long debounceDelay = 50; // Intervalo entre leituras válidas de cada pulsos
 
-Ticker Contar;
+bool wifiSt = false; // Estado da conexão WIFI(teste)
+bool ready = false;  // Indica se há dados a serem enviados
 
-Ticker PushFila;
+QueueList<data> dataQ; // Lista FIFO
 
-Ticker EmptyFila;
+Ticker PushData;
+Ticker SendQ;
 
-void FuncContar()
-{
-  cont+= random(1,5);
+void pulseRead()
+{                                        // Leitura de pulsos do sensor
+  static unsigned long lastDebounce = 0; // Registra a última marcação de tempo
+  unsigned long debounce = millis();     // Tempo atual
+  if (debounce - lastDebounce > debounceDelay)
+  { // Se intervalo entre chamadas for maior que intervalo de leituras válidas
+    pulseCounter++;
+    Serial.print("\t \t \t +1 \t"); // (Teste)
+    Serial.println(debounce);
+  }
+  lastDebounce = debounce; // Reseta a última marcação de tempo
 }
 
-void FuncPush()
-{
-  dados d;
-  d.valor = cont;
-  d.time = millis() / 1000;
-  filaPulsos.push(d);
-  cont = 0;
+void pushQ()
+{ // Preenche a fila
+  if (pulseCounter > 0)
+  { // Apenas se houverem dados
+    data d;
+    d.value = pulseCounter;
+    d.time = millis() / 1000; // (Teste) Pegar a hora do SAIOT <<< CORRIGIR
+    dataQ.push(d);            // Armazena os dados na lista
+    pulseCounter = 0;         // Reseta contador
+    ready = true;             // Indica existência de dados prontos para envio
+  }
 }
 
-void FuncEmpty()
+void popQ()
 {
-  while (filaPulsos.count() > 0)
-  {
-    dados x = filaPulsos.pop();
-    Serial.print(x.valor);
-    Serial.print('\t');
-    Serial.println(x.time);
+  if (ready && !wifiSt) // (Teste)
+    Serial.println("waiting for wifi");
+  if (wifiSt && ready)
+  { // Se houverem dados prontos e WIFI disponível
+    while (dataQ.count() > 0)
+    {                          // Enquanto houverem elementos na fila
+      data out = dataQ.pop();  // Retira o primeiro dado da fila
+      Serial.print(out.value); // Envia
+      Serial.print('\t');
+      Serial.println(out.time);
+    }
+    ready = false; // Reseta a flag
   }
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("Comecou");
-  // put your setup code here, to run once:
-  //interrupcao por tempo para gravar na flash
-  Contar.attach(tempCont, FuncContar);
-  PushFila.attach(tempPush, FuncPush);
+  pinMode(SENSOR_PIN, INPUT_PULLUP); // Configura porta do sensor
 
-  EmptyFila.attach(tempEmpty, FuncEmpty);
+  Serial.begin(115200); // (Teste)
+  Serial.println();
+  Serial.println("Comecou");
+  // Associa interrupção ext ao pino do sensor
+  attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), pulseRead, RISING);
+
+  PushData.attach(PUSH_INTERVAL, pushQ); // Inicializa timer de armazenamento 
+  SendQ.attach(SEND_INTERVAL, popQ); // Inicializa timer de envio
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) // (Teste) Envie 'w' pela serial para simular o "status do wifi"
+  {
+    char c = Serial.read();
+    if (c == 'w')
+      wifiSt = !wifiSt;
+  }
 }
